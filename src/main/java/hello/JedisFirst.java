e99f37ebd2add53444a9dbfbd0f274cb233bbdb8
berry <zhu95yuan@163.com>
2020-03-03 20:42:56 +0800

    just test.

--- src/main/java/hello/JedisFirst.java
+++ src/main/java/hello/JedisFirst.java
@@ -3,6 +3,8 @@ package hello;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.crypto.spec.DESedeKeySpec;
+
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
@@ -11,10 +13,11 @@ public class JedisFirst {
 	public static void main(String[] args) {
 		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
 		//Jedis Cluster will attempt to discover cluster nodes automatically
-		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 7379));
+		jedisClusterNodes.add(new HostAndPort("172.20.10.3", 7000));
 		JedisCluster jc = new JedisCluster(jedisClusterNodes);
 		jc.set("foo", "bar");
 		String value = jc.get("foo");
+		String s = null;
 
 	}
 
--- src/main/java/hello/T2.java
+++ src/main/java/hello/T2.java
@@ -1,9 +1,18 @@
 package hello;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class T2 {
 	public void test() {
 		System.out.println(TBerry.class.getName());
 		System.out.println("可以了！");
 	}
 	
+	public static void main(String[] args) {
+		 Date date = new Date();
+		 Calendar c1 = Calendar.getInstance();
+		 int day = c1.getFirstDayOfWeek();
+		 System.out.println(day);
+	}
 }
